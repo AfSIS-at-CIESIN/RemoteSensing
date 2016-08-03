ebe69f9cf9cdb26ec7f573d9b6c14fb59f985fb7
johnsquires <jsquires@ciesin.columbia.edu>
2016-08-03 17:43:50 -0400

    Added getopts flags and help
    Inputs are now managed through getopts. A help/usage (-h) flag was
    added. Some checks to make sure required arguments and values were
    passed.

--- ClipAndReproject/clip_rasters.sh
+++ ClipAndReproject/clip_rasters.sh
@@ -1,25 +1,71 @@
 #!/bin/bash
 # clip_rasters.sh
-# written by: Kimberly Peng
+# written by: Kimberly Peng, 
+# modified by: John Squires
 # Clips rasters for geographic and lambert azimuthal equal area geotiffs.
 
 #Sample Usage
 #/data4/afsisdata/IRI_MODIS/scripts/./clip_rasters.sh /data4/AfSIS2tifs/SRTM/SRTM_AfricaClip_LAEA /data4/afsisdata/IRI_MODIS/scripts/test 30000 laeaKPtemp trmm
 
-ClipDir=$1 #directory containing clip masks
-InputDir=$2 #directory containing geotiffs to be clipped
-Resolution=$3 #resolution of the geotiffs
-location=$4 #grass location in which the geotiffs will be imported and clipped
-mapset=$5 #grass location in which the geotiffs will be imported and clipped
+usage()
+{
+cat << EOF
+USAGE: $0 options
+
+This script clips geotiffs to a clip mask.
+Can be used for geographic and lambert azimuthal equal area geotiffs.
+
+OPTIONS:
+   -h      Show this message
+   -c      Directory containing clip masks (required)
+   -i      Input directory of geotiffs to be clipped (required)
+   -l      GRASS location (required)
+   -m      GRASS mapset (required)
+   -r      Resolution of data in meters (required)
+             example -r 500 (for 500 meter resolution)
+EOF
+}
+
+# declare globals and assign variables through getopts
+ClipDir=
+InputDir=
+Resolution=
+location=
+mapset=
+
+while getopts “hc:i:l:m:r:” OPTION
+do
+    case $OPTION in
+    	c) ClipDir=$OPTARG
+            ;;
+        h) usage
+            exit 1
+            ;;
+        i) InputDir=$OPTARG
+			;;
+		l) location=$OPTARG
+			;;
+		m) mapset=$OPTARG
+			;;
+		r) Resolution=$OPTARG
+			;;	
+        ?) usage
+           exit
+           ;;
+     esac
+done
+
+# 5 required arguments, check that they were provided
+if [[ -z "$ClipDir" ]] || [[ -z "$InputDir" ]] || [[ -z "$location" ]] || \
+   [[ -z "$mapset" ]] || [[ -z "$Resolution" ]]; then
+	printf '\nERROR: You need to supply the flags and values for each required argument.\n'
+	usage
+	exit 1
+fi
 
 #####Start grass environment#####
 #some settings:
 TMPDIR=/tmp
-
-# directory of our software and grassdata:
-#MAINDIR=/
-# our private /usr/ directory:
-#MYUSER=$MAINDIR/Users/username/
 
 # path to GRASS binaries and libraries:
 export GISBASE=/usr/lib/grass64
@@ -46,15 +92,11 @@ export LD_LIBRARY_PATH=$GISBASE/lib:$LD_LIBRARY_PATH
 # use process ID (PID) as lock file number:
 export GIS_LOCK=$$
 
-# this should print the GRASS version used:
-g.version 
-#g.gisenv -n
-
 #####Import clip file and set as mask#####
 #change to the directory containing the clip file
 cd $ClipDir
 
-#add meter to resolution so that 500 and 5000 aren't found with * operator
+#add meter to resolution so that 500 AND 5000 aren't found with * operator
 Resolution+="m"
 
 clipfile=$(ls *"$Resolution"*)
