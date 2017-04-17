a6f08043d7a17859e845dc210cf939b9ff4bc027
johnsquires <jsquires@ciesin.columbia.edu>
2017-04-17 16:19:02 -0400

    removes setting nulls and refill logic
    removes logic at end of script that set null values to 0 and then
    refilled 249-255 values from fill-raster.

--- MODIS-USGS/LAI-FPAR/laifpar_annual.sh
+++ MODIS-USGS/LAI-FPAR/laifpar_annual.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# written by Kimberly Peng, modified by Yanni Zhan and John Squires
 
 #/data4/afsisdata/USGS_updates/scripts/./laifpar_annual.sh /data1/afsisdata/MODIS/LAI_FPAR_MCD15A2/Fpar MCD15A2 Fpar Geographic mcd15a2Fpar
 #/data4/afsisdata/USGS_updates/scripts/./laifpar_annual.sh /data1/afsisdata/MODIS/LAI_FPAR_MOD15A2/Fpar MOD15A2 Fpar Geographic mod15a2Fpar
@@ -13,11 +14,6 @@ Mapset=$5
 ######Starting GRASS Environment
 #some settings:
 TMPDIR=/tmp
-
-# directory of our software and grassdata:
-#MAINDIR=/
-# our private /usr/ directory:
-#MYUSER=$MAINDIR/Users/username/
 
 # path to GRASS binaries and libraries:
 export GISBASE=/usr/lib/grass64
@@ -67,9 +63,9 @@ echo start year is $StartYear
 EndYear=$(echo $lastObs | cut -c1-4)
 echo last year is $EndYear
 
-# echo $list
-####################################################################
+###
 #specify g.region based gdalinfo stats on one of the continent mosaics
+###
 g.region -p n=39:59:59.999986N s=40:00:00.460219S e=78:19:31.27594E w=26:06:29.324819W rows=9600 cols=12532 res=0:00:30.000048
 
 OutputDir=$InputDir/outputs
@@ -94,27 +90,7 @@ do
 	if [ $DatasetName == "Fpar" ] ; then
 		rslist=$(echo $yearlist | sed "s/ /,/g;s/.tif/@"$Mapset"/g")
 		r.series input=$rslist output=FprAverage_"$i"@"$Mapset" method=average
-
-		#fill null values with zero
-		r.null map=FprAverage_"$i"@"$Mapset" null=0
-
-		if [ $NasaCode == "MCD15A2" ]; then
-			#import fill value file
-			r.in.gdal input=/data4/afsisdata/USGS_updates/laifpar/fill_value_rasters/geographic/MCD15A2_Fpar_2002_2014_AvgFills.tif output=MCD15A2_Fpar_2002_2014_AvgFills -e
-
-			#restore the fill values 249-255
-			r.mapcalculator amap=FprAverage_"$i"@"$Mapset" bmap='MCD15A2_Fpar_2002_2014_AvgFills@'$Mapset formula="if(A,A,A+B)" outfile="$NasaCode"_"$i"_'FprAverage'
-		fi
-		if [ $NasaCode == "MOD15A2" ]; then
-			#import fill value file
-			r.in.gdal input=/data4/afsisdata/USGS_updates/laifpar/fill_value_rasters/geographic/MOD15A2_Fpar_2000_2014_AvgFills.tif output=MOD15A2_Fpar_2000_2014_AvgFills -e
-
-			#restore the fill values 249-255
-			r.mapcalculator amap=FprAverage_"$i"@"$Mapset" bmap='MOD15A2_Fpar_2000_2014_AvgFills@'$Mapset formula="if(A,A,A+B)" outfile="$NasaCode"_"$i"_'FprAverage'
-		fi
-		
-		#exports to geotiff
-		r.out.gdal input="$NasaCode"_"$i"_'FprAverage@'$Mapset output=$OutputDir/"$NasaCode"_"$DatasetName"_"$i"_'Average.tif' type=Float32 
+		r.out.gdal input=FprAverage_"$i"@"$Mapset" output=$OutputDir/"$NasaCode"_"$DatasetName"_"$i"_'Average.tif' type=Float32 
 	fi
 	i=$((i+1))
 done
